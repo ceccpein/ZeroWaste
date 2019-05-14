b540e1efb7026007226d12e653ba74147f8c0613
ceccpein <cecilia.peinert@gmail.com>
2019-05-14 16:23:35 -0700

    worked on login

--- app/src/main/java/com/example/zerowaste/MainActivity.java
+++ app/src/main/java/com/example/zerowaste/MainActivity.java
@@ -5,24 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private static EditText username;
+    private static EditText password;
+    private static Button login_btn;
+    Button loginBtn = (Button) findViewById(R.id.loginButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonClick(loginBtn);
     }
-
-    Button loginBtn = (Button) findViewById(R.id.loginButton);
 
     public void buttonClick(Button loginBtn) {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(MainActivity.this, MyFridge.class);
-                startActivityForResult(myIntent, 100);
+                startActivity(myIntent);
             }
         });
     }
