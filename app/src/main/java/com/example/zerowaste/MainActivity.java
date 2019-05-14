beb40b868f53f9528200bafa5862dcf6d65e1c79
ceccpein <cecilia.peinert@gmail.com>
2019-05-14 14:15:17 -0700

    added myFridge to login branch

--- app/src/main/java/com/example/zerowaste/MainActivity.java
+++ app/src/main/java/com/example/zerowaste/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.zerowaste;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(ImageView imgbtn) {
+        imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, myFridge.class);
+                startActivityForResult(myIntent, 100);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/zerowaste/MyFridge.java
@@ -0,0 +1,15 @@
+package com.example.zerowaste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyFridge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fridge);
+    }
+
+
+}
