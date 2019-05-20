e22e816d14a5d8fb007ff2e976a861401a4a3bec
IngvildLoeesNilsson <ingvild-@hotmail.com>
2019-05-20 14:32:09 -0700

    able to add stuff to firebase2

--- app/src/main/java/com/example/zerowaste/MyFridge.java
+++ app/src/main/java/com/example/zerowaste/MyFridge.java
@@ -1,22 +1,36 @@
 package com.example.zerowaste;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import android.os.Handler;
+
+
+
 
 public class MyFridge extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     private ValueEventListener postListener;
-
+    public String foods = "empty";
+    ArrayAdapter<String> arrayAdapter;
     String TAG = "tag12346";
 
     @Override
@@ -31,8 +45,66 @@ public class MyFridge extends AppCompatActivity {
         //goes into users, ingvild, matvarer, bananas og setter bananas med 10 mai
         //mDatabase.child("posts").child("author").setValue(new MainActivity.Post("ingvilds2","nilsson2"));
         //addGrocery("ost","15 mai");
-        addGrocery("osten","juni");
-        readDB();
+
+        //foods = readDB();
+
+        final List<String> dataList = new ArrayList<String>();
+        final ListView listview = (ListView) findViewById(R.id.listview);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataList);
+
+        addGrocery("melk","10 juni");
+        addGrocery("melk1","10 juni");
+        addGrocery("melk2","10 juni");
+        addGrocery("melk3","10 juni");
+        addGrocery("melk4","10 juni");
+        addGrocery("mel5","10 juni");
+        addGrocery("mel6","10 juni");
+        addGrocery("melk7","10 juni");
+        addGrocery("melk8","10 juni");
+
+
+
+        readData(new MyCallback() {
+            @Override
+            public void onCallback(String value) {
+
+                value = value.substring(1,value.length()-1);
+                String[] allItems1 = value.split(",");
+                ArrayList<String> allItems = new ArrayList<String>(Arrays.asList(allItems1));
+                for (String item : allItems) {
+                    String[] foodexp = item.split("=");
+                    String food = foodexp[0];
+                    String exp = foodexp[1];
+                    dataList.add(food + "  :  " + exp);
+                }
+
+
+                listview.setAdapter(arrayAdapter);
+                listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        listview.removeViewAt(position);
+                    }
+                });
+            }
+        });
+
+
+    }
+
+
+    public ArrayList<String> addFoodToListView(String list) {
+        ArrayList<String> returnlist = null;
+        String[] allItems1 = list.split(",");
+        ArrayList<String> allItems = new ArrayList<String>(Arrays.asList(allItems1));
+        for (String item : allItems) {
+            String[] foodexp = item.split("=");
+            String food = foodexp[0];
+            String exp = foodexp[1];
+            returnlist.add(food + "  :  " + exp);
+
+        }
+        return returnlist;
     }
 
     public void addGrocery(String grocery, String date) {
@@ -42,33 +114,26 @@ public class MyFridge extends AppCompatActivity {
 
 
 
-    public static class Post {
 
-        public String author;
-        public String title;
-
-        public Post(String author, String title) {
-            this.author = author;
-            this.title = title;
-        }
-
-    }
-
-    // Get a reference to our posts
-
-    public void readDB() {
+    public String readDB() {
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("/users/ingvild/matvarer");
-
+        final String s = "unitialized";
         // Attach a listener to read the data at our posts reference
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                Log.d(TAG, dataSnapshot.getKey().toString());
-                String s = dataSnapshot.getValue().toString();
-                Log.d(TAG, s);
+
+                    //Log.d(TAG, dataSnapshot.getKey().toString());
+                    String s = dataSnapshot.getValue().toString();
+                    foods = s;
+                    Log.d(TAG,"here is foods2"+s);
+
+
+
+
             }
 
             @Override
@@ -76,9 +141,35 @@ public class MyFridge extends AppCompatActivity {
                 // Failed to read value
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
-        });}
+        });
+
+    return s;
+    }
+
+
+    public void readData(final MyCallback myCallback) {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("/users/ingvild/matvarer");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue().toString();
+                myCallback.onCallback(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+    }
 
 
 
+    public interface MyCallback {
+        void onCallback(String value);
+    }
 
 }
+
+
+
+
